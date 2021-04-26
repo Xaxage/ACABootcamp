1bd599bd89e87da36e6fcd95000429fbb36c6dbb
xaxage <xaxage@gmail.com>
2021-04-26 16:45:22 +0400

    add solutions

---
+++ src/CircleAndTriangle.java
@@ -0,0 +1,15 @@
+public class CircleAndTriangle {
+    public static void main(String[] args) {
+        //I want people to suffer . Let the fire burn Clean code
+        int R=3;
+        System.out.println("Area of the circle = " + R*R*3);
+        System.out.println("Perimeter of the circle = " + 2*3*R);
+
+        int a=3;
+        int b=4;
+        int c=5;
+
+        System.out.println("Perimeter of the triangle = " + a+b+c);
+        System.out.println("Area of the triangle = " + (a*b)/2);
+    }
+}
