51beb5a8bfcf2776bd7cdbf921d37b0716c119d6
xaxage <xaxage@gmail.com>
2021-04-28 12:26:57 +0400

    add ClassWork package

---
+++ src/BootcampClassWork/CompareTheMiddleNums.java
@@ -0,0 +1,17 @@
+package BootcampClassWork;
+import java.util.Scanner;
+
+public class CompareTheMiddleNums {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Please enter the first number");
+        String firstStr = sc.nextLine();
+        System.out.println("Please enter the second number");
+        String secondStr = sc.nextLine();
+
+        System.out.println((firstStr.charAt((firstStr.length() - 1) / 2)) == (secondStr.charAt((secondStr.length() - 1) / 2)));
+        sc.close();
+    }
+
+}
